7c3dff379e349a36e578fa6e24e65637c888c91f
tys-hiroshi <tashiro.hiroshi@toyoko-sys.co.jp>
2021-12-08 15:10:02 +0900

    mod

--- newbitcoin/newbitcoin/code-ch01/exercise.py
+++ newbitcoin/newbitcoin/code-ch01/exercise.py
@@ -8,3 +8,14 @@
 ## (59 + 49) % 57 = 108 % 57 = 51
 ## 52 − 30 − 38
 # (22 - 38) % 57 = -16 % 57 = 41
+
+from ecc import FieldElement
+# a = FieldElement(7, 13)
+# b = FieldElement(12, 13)
+# c = FieldElement(6, 13)
+# print(a+b==c)
+
+a = FieldElement(3, 13)
+b = FieldElement(12, 13)
+c = FieldElement(10, 13)
+print(a*b==c)
