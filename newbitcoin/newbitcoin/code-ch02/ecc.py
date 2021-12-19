8c373a061268cbb63d92b8f2521dea6b7c723fa8
Hiroshi Tashiro <tashiro.hiroshi@toyoko-sys.co.jp>
2021-12-19 23:31:12 +0900

    Update ecc.py

--- newbitcoin/newbitcoin/code-ch02/ecc.py
+++ newbitcoin/newbitcoin/code-ch02/ecc.py
@@ -409,3 +409,46 @@ class PrivateKey:
         if s > N/2:
             s = N - s
         return Signature(r, s)
+
+from helper import hash256
+
+class Tx:
+
+    def __init__(self, version, tx_ins, tx_outs, locktime, testnet=False):
+        self.version = version
+        self.tx_ins = tx_ins
+        self.tx_outs = tx_outs
+        self.locktime = locktime
+        self.testnet = testnet
+
+    def __repr__(self):
+        tx_ins = ''
+        for tx_in in self.tx_ins:
+            tx_ins += tx_in.__repr__() + '\n'
+        tx_outs = ''
+        for tx_out in self.tx_outs:
+            tx_outs += tx_out.__repr__() + '\n'
+        return 'tx: {}\nversion: {}\ntx_ins:\n{}tx_outs:\n{}locktime: {}'.format(
+            self.id(),
+            self.version,
+            tx_ins,
+            tx_outs,
+            self.locktime,
+        )
+
+    def id(self):
+        '''人が読める16進数表記のトランザクションハッシュ'''
+        return self.hash().hex()
+
+    def hash(self):
+        '''バイナリ形式のハッシュ'''
+        return hash256(self.serialize())[::-1]
+
+
+    @classmethod
+    def parse(cls, serialization):
+        version = serialization[0:4]
+
+    @classmethod
+    def parse(cls, stream):
+        serialized_version = stream.read(4)
