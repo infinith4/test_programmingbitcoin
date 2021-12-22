a8d47e5dc0fe07baf1597c808c9acb22144f8d16
Hiroshi Tashiro <tashiro.hiroshi@toyoko-sys.co.jp>
2021-12-22 16:04:39 +0900

    add op

--- newbitcoin/newbitcoin/code-ch02/ecc.py
+++ newbitcoin/newbitcoin/code-ch02/ecc.py
@@ -453,10 +453,19 @@ class Tx:
     def parse(cls, stream):
         serialized_version = stream.read(4)
 
-    @classmethod
-    def parse(cls, s, testnet=False):
-        version = little_endian_to_int(s.read(4))
-        return cls(version, None, None, None, testnet=testnet)
+    # @classmethod
+    # def parse(cls, s, testnet=False):
+    #     version = little_endian_to_int(s.read(4))
+    #     return cls(version, None, None, None, testnet=testnet)
+
+    # @classmethod
+    # def parse(cls, s, testnet=False):
+    #     version = little_endian_to_int(s.read(4))
+    #     num_inputs = read_varint(s)
+    #     inputs = []
+    #     for _ in range(num_inputs):
+    #         inputs.append(TxIn.parse(s))
+    #     return cls(version, inputs, None, None, testnet=testnet)
 
     @classmethod
     def parse(cls, s, testnet=False):
@@ -465,7 +474,12 @@ class Tx:
         inputs = []
         for _ in range(num_inputs):
             inputs.append(TxIn.parse(s))
-        return cls(version, inputs, None, None, testnet=testnet)
+        num_outputs = read_varint(s)
+        outputs = []
+        for _ in range(num_outputs):
+            outputs.append(TxOut.parse(s))
+        locktime = little_endian_to_int(s.read(4))
+        return cls(version, inputs, outputs, locktime, testnet=testnet)
 
 
 def little_endian_to_int(b):
@@ -477,8 +491,25 @@ def int_to_little_endian(n, length):
     返す'''
     return n.to_bytes(length, 'little')
 
+from script import Script
 
 class TxIn:
+    
+    def __init__(self, prev_tx, prev_index, script_sig=None, sequence=0xffffffff):
+        self.prev_tx = prev_tx
+        self.prev_index = prev_index
+        if script_sig is None:
+            self.script_sig = Script()
+        else:
+            self.script_sig = script_sig
+        self.sequence = sequence
+
+    def __repr__(self):
+        return '{}:{}'.format(
+            self.prev_tx.hex(),
+            self.prev_index,
+        )
+
     @classmethod
     def parse(cls, s):
         '''Takes a byte stream and parses the tx_input at the start.
@@ -489,3 +520,21 @@ class TxIn:
         script_sig = Script.parse(s)
         sequence = little_endian_to_int(s.read(4))
         return cls(prev_tx, prev_index, script_sig, sequence)
+
+class TxOut:
+
+    def __init__(self, amount, script_pubkey):
+        self.amount = amount
+        self.script_pubkey = script_pubkey
+
+    def __repr__(self):
+        return '{}:{}'.format(self.amount, self.script_pubkey)
+
+    @classmethod
+    def parse(cls, s):
+        '''Takes a byte stream and parses the tx_output at the start.
+        Returns a TxOut object.
+        '''
+        amount = little_endian_to_int(s.read(8))
+        script_pubkey = Script.parse(s)
+        return cls(amount, script_pubkey)
