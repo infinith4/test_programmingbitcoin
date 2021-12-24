c66d9a652c53060c20c1f3c0f6db8655fd2dff53
Hiroshi Tashiro <tashiro.hiroshi@toyoko-sys.co.jp>
2021-12-24 16:12:18 +0900

    Update ecc.py

--- newbitcoin/newbitcoin/code-ch02/ecc.py
+++ newbitcoin/newbitcoin/code-ch02/ecc.py
@@ -1,3 +1,4 @@
+from _typeshed import BytesPath
 from unittest import TestCase
 from helper import encode_varint
 
@@ -504,6 +505,7 @@ def int_to_little_endian(n, length):
     return n.to_bytes(length, 'little')
 
 from script import Script
+from io import BytesIO
 
 class TxIn:
     
@@ -564,3 +566,35 @@ class TxOut:
         result = int_to_little_endian(self.amount, 8)
         result += self.script_pubkey.serialize()
         return result
+
+# get transaction info
+class TxFetcher:
+    cache = []
+
+    @classmethod
+    def get_url(cls, testnet=False):
+        if testnet:
+            return 'http://testnet.programmingbitcoin.com'
+        else:
+            return 'http://mainnet.programmingbitcoin.com'
+
+    @classmethod
+    def fetch(cls, tx_id, testnet=False, fresh=False):
+        if fresh or (tx_id not in cls.cache):
+            url = '{}/tx/{}.hex'.format(cls.get_url(testnet), tx_id)
+            response = requests.get(url)
+            try:
+                raw = bytes.fromhex(response.text.strip())
+            except ValueError:
+                raise ValueError('unexpected response: {}'.format(response.text))
+            if raw[4] == 0:
+                raw = raw[:4] + raw[6:]
+                tx = Tx.parse(BytesIO(raw), testnet=testnet)
+                tx.locktime = little_endian_to_int(raw[-4:])
+            else:
+                tx = Tx.parse(BytesIO(raw), testnet=testnet)
+            if tx.id() != tx_id:
+                raise ValueError('not the same id: {} vs {}'.format(tx.id(), tx_id))
+            cls.cache[tx_id] = tx  # third parties を信用しない
+        cls.cache[tx_id].testnet = testnet
+        return cls.cache[tx_id]
